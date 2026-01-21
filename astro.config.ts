85401a0254cfb1bf5f6d7bb1de7f0bb0a91544c7
awahdat <51273010+awahdat@users.noreply.github.com>
2026-01-20 22:36:16 -0500

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
-import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   // used to generate images
@@ -12,7 +11,7 @@ export default defineConfig({
       ? `https://${process.env.VERCEL_URL}/`
       : 'https://localhost:3000/',
   trailingSlash: 'ignore',
-  integrations: [mdx(), sitemap(), UnoCSS({ injectReset: true })],
+  integrations: [sitemap(), UnoCSS({ injectReset: true })],
   vite: {
     optimizeDeps: {
       exclude: ['@resvg/resvg-js'],
